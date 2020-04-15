b668dfca0020889e5e957a3df7be3d64cff81666
learnToCodeJas <39787338+learnToCodeJas@users.noreply.github.com>
2020-04-15 16:21:01 +0530

    cOMMIT 1

--- force-app/main/default/lwc/test/test.js
+++ force-app/main/default/lwc/test/test.js
@@ -3,6 +3,6 @@ import { LightningElement } from 'lwc';
 export default class Test extends LightningElement {
     greeting = 'World';
     changeHandler(event) {
-      this.greeting = event.target.value;
+      this.greeting  = event.target.value ;
     }
 }
